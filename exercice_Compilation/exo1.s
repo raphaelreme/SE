7fb261d5ec7a39881c38732a7f7e5bab3a927319
Raphael REME <raphael.reme@telecom-paristech.fr>
2018-09-27 18:49:27 +0200

    Correction de l'exo, reste encore pas mal de doutes a eclaircir

--- exercice_Compilation/exo1.s
+++ exercice_Compilation/exo1.s
@@ -1,19 +1,17 @@
-ldr r1,=a
-ldr r1, [r1]
 bl f
 
 end:
   b end
 
 f:
+  ldr r2, =a
+  ldr r1, [r2]
   mov r0, #0x00
 
   loop:
     cmp r0,r1
     bxgt lr @fin du for et donc de f.
     bl g
-    add r0,r0,#0x01
+    ldr r1, [r2]
+    add r0,r0,#1
     b loop
-
-g:
-  bx lr
