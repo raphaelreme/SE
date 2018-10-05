bf427d46b67aaee141d2e3e75f02b5fda8b8ff53
Raphael REME <raphael.reme@orange.fr>
2018-10-05 10:40:08 +0200

    TP : debut Fibonnaci

--- TP/t.c
+++ TP/t.c
@@ -1,5 +1,16 @@
+int fibo(int);
+
+
 int main(){
- while (1) {
- }
- return 0;
+ fibo(8);
+}
+
+int fibo(int n){
+  if (n<0){
+    return -1;
+  }
+  if (n<2){
+    return n;
+  }
+  return fibo(n-1)+fibo(n-2);
 }
