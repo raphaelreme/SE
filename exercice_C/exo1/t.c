b1bb5237c73ea50e0b7b81fb28194da736be31f6
Raphael REME <raphael.reme@orange.fr>
2018-10-05 13:51:26 +0200

    Nouvelles reponses sur ce qu'est vraiment la premiere et troisieme colonne ! Mais il semblerait que la premiere ne soit pas vraiment la taille. A suivre..

--- exercice_C/exo1/t.c
+++ exercice_C/exo1/t.c
@@ -2,15 +2,32 @@
 #include <stdio.h>
 
 int32_t x = 34;
+
+int64_t y64;
 int32_t y32;
 int16_t y16;
 int8_t y8;
-int64_t y64;
-int y_int;
-short y_short;
+
 char y_char;
+short y_short;
+int y_int;
 long y_long;
 double y_double;
+
+int8_t y8_tab_taille20[20];
+int16_t y16_tab_taille20[20];
+int32_t y32_tab_taille20[20];
+int64_t y64_tab_taille20[20];
+
+int8_t y8_tab_taille10[10];
+int16_t y16_tab_taille10[10];
+int32_t y32_tab_taille10[10];
+int64_t y64_tab_taille10[10];
+
+int8_t* y8_pointeur;
+int16_t* y16_pointeur;
+int32_t* y32_pointeur;
+int64_t* y64_pointeur;
 
 
 const char mesg[] = "Hello World!";
