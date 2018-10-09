0ff8a08962c47defed229090f79fefa4c3627072
Raphael REME <raphael.reme@telecom-paristech.fr>
2018-10-09 16:32:16 +0200

    Ajout de la fonction led, reste a la tester dans main !

--- TP/led.c
+++ TP/led.c
@@ -3,6 +3,7 @@
 void led_init(){
   CLOCK_ENA = CLOCK_ENA | (3<<1); //enable port B and C
   GPIOB_MODER = (GPIOB_MODER & ~(0x3<<28)) | (1<<28);
+  GPIOC_MODER = (GPIOC_MODER & ~(0x3<<28));
 }
 
 void led_g_on(){
@@ -10,5 +11,18 @@ void led_g_on(){
 }
 
 void led_g_off(){
+  GPIOB_BSRR = 1<<30;
+}
+
+void led(enum state s){
+  if (s==LED_OFF){
+    GPIOC_MODER = GPIOC_MODER & ~(0x3<<28);
+    return;
+  }
+  GPIOC_MODER = (GPIOC_MODER & ~(0x3<<28)) | (1<<28);
+  if (s==LED_YELLOW){
+    GPIOB_BSRR = 1<<14;
+    return;
+  }
   GPIOB_BSRR = 1<<30;
 }
--- TP/led.h
+++ TP/led.h
@@ -11,9 +11,12 @@
 #define GPIOC_MODER (*(volatile uint32_t *)0x48000800)
 #define GPIOC_BSRR (*(volatile uint32_t *)0x48000818)
 
+enum state {LED_OFF, LED_YELLOW, LED_BLUE};
+
 
 void led_init(void);
 void led_g_on(void);
 void led_g_off(void);
+void led(enum state);
 
 #endif
