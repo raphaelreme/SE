47973bc9a2212475931c4ef4558fa129cbb28453
Raphael REME <raphael.reme@orange.fr>
2018-10-12 11:53:39 +0200

    Fin de TP, entrain de faire l'initialisation de usart1

--- TP/uart.c
+++ TP/uart.c
@@ -1,4 +1,20 @@
+#include "uart.h"
+#include "stm32l475xx.h"
+#include "stm32l4xx.h"
+
 
 void uart_init(){
+  //Ena clock of port B
+  SET_BIT(RCC->AHB2ENR,RCC_AHB2ENR_GPIOBEN);
+
+  //set alternative fuction mode for TX and RX
+  MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODE6_Msk, GPIO_MODER_MODE6_1);
+  MODIFY_REG(GPIOB->MODER, GPIO_MODER_MODE7_Msk, GPIO_MODER_MODE7_1);
+
+  //Set the function to AF7 for both of them
+  MODIFY_REG(GPIOB->AFRL,GPIO_AFRL_AFSEL6_Msk|GPIO_AFRL_AFSEL7_Msk, (~GPIO_AFRL_AFSEL6_3)|(~GPIO_AFRL_AFSEL7_3));
+
+  //Ena the clock of USART1
+  USART1->CR1
   
 }
