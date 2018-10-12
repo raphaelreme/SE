9996d8a718270d283a59deb548dd6c1dbe0edc0c
Raphael REME <raphael.reme@orange.fr>
2018-10-12 11:53:07 +0200

    Suppression des definitions de registres inutiles dans le header

--- TP/led.h
+++ TP/led.h
@@ -3,16 +3,7 @@
 
 #include <stdint.h>
 
-#define CLOCK_ENA (*(volatile uint32_t *)0x4002104c)
-
-#define GPIOB_MODER (*(volatile uint32_t *)0x48000400)
-#define GPIOB_BSRR (*(volatile uint32_t *)0x48000418)
-
-#define GPIOC_MODER (*(volatile uint32_t *)0x48000800)
-#define GPIOC_BSRR (*(volatile uint32_t *)0x48000818)
-
 enum state {LED_OFF, LED_YELLOW, LED_BLUE};
-
 
 void led_init(void);
 void led_g_on(void);
