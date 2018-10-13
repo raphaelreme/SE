3066619090b6c7c14f68fd7063c2d582675f7fb7
Raphael REME <raphael.reme@orange.fr>
2018-10-13 12:47:44 +0200

    ajout de uart au Makefile

--- TP/main.c
+++ TP/main.c
@@ -1,10 +1,28 @@
 #include "led.h"
 #include "clocks.h"
+#include "uart.h"
 
 int fibo(int);
+void led_trial(void);
 
 int main(){
   clocks_init();
+
+  led_trial();
+}
+
+
+int fibo(int n){
+  if (n<0){
+    return -1;
+  }
+  if (n<2){
+    return n;
+  }
+  return fibo(n-1)+fibo(n-2);
+}
+
+void led_trial(){
   led_init();
 
   for (int i=0; i<10;i++){
@@ -24,15 +42,4 @@ int main(){
     led(LED_BLUE);
   }
   led(LED_OFF);
-}
-
-
-int fibo(int n){
-  if (n<0){
-    return -1;
-  }
-  if (n<2){
-    return n;
-  }
-  return fibo(n-1)+fibo(n-2);
 }
