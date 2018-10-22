4d6c6ff18d18dee50f550b22172939783d438d28
Raphael REME <raphael.reme@orange.fr>
2018-10-22 15:37:19 +0200

    Rajout du programme d'affichage d'image

--- TP/main.c
+++ TP/main.c
@@ -3,7 +3,11 @@
 #include "led.h"
 #include "irq.h"
 #include "buttons.h"
+#include "matrix.h"
 
+extern uint8_t _binary_image_raw_start;
+
+static void test_static_image(void);
 
 void wait(int n){
   for (int j=0; j<n; j++){
@@ -16,4 +20,21 @@ int main(){
   led_init();
   irq_init();
   button_init();
+  matrix_init();
+  test_static_image();
+}
+
+
+/*
+ * Display image.raw on the leds.
+ * Assume that image.raw has 8*8*3 bytes of data.
+ */
+static void test_static_image(){
+  uint8_t * start = &_binary_image_raw_start;
+  while (1){
+    for (int i=0; i<8; i++){
+      mat_set_row(i, (rgb_color *)(start+i*24));
+      wait(1000);
+    }
+  }
 }
